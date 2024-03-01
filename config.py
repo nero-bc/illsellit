7edc9a7b5d0eb6fbd3e2dcc58e4457650f45a69d
rohit87099 <155307408+rohit87099@users.noreply.github.com>
2024-03-01 16:11:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,13 +43,13 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\noHhh mutthal</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("Marega Muthi ✊✊", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('TRUE', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('TRUE', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("False", True) == 'False'
